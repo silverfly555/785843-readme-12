a9c6e8ad0f40bb19b59a940b5bdb62be933e7ecc
Vladimir <silverfly@mail.ru>
2020-12-07 11:00:46 +0300

    Начинаем внедрение функции показа даты

--- index.php
+++ index.php
@@ -19,6 +19,34 @@ function text($contents, $quantity=300)
     return $contents;
     }
 }
+function time_print($time)
+{
+      //если до текущего времени прошло меньше 60 минут
+      if ($time_now-$time<60){
+        $result=$time_now-$time
+        return $result.' минут назад';
+      }
+      //если до текущего времени прошло больше 60 минут, но меньше 24 часов
+      elseif ($time_now-$time>60 && $time_now-$time<24 часа) {
+        $result=
+        return $result.' часов назад';
+      }
+      //если до текущего времени прошло больше 24 часов, но меньше 7 дней
+      elseif ($time_now-$time>24 часов && $time_now-$time<7 дней) {
+        $result=
+        return $result.' дней назад';
+      }
+      //если до текущего времени прошло больше 7 дней, но меньше 5 недель
+      elseif ($time_now-$time>7 дней && $time_now-$time<5 недель) {
+        $result=
+        return $result.' недель назад';
+      }
+      //если до текущего времени прошло больше 5 недель
+      elseif ($time_now-$time>5 недель) {
+        $result=
+        return $result.' месяцев назад';
+      }
+}
 $posts = [
 [
 'title'=>'Цитата',
